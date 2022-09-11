1e55f9f2334160b9e553d304aa5ed3a6e95c17ce
choeqq <k.chola@op.pl>
2022-09-11 18:43:44 +0200

    jwt creation, and verification done

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,8 +1,34 @@
-import { UserModel } from "../schema/user.schema";
+import { ApolloError } from "apollo-server";
+import { CreateUserInput, LoginInput, UserModel } from "../schema/user.schema";
+import Context from "../types/context";
+import bcrypt from "bcrypt";
+import { signJwt } from "../utils/jwt";
 
 class UserService {
-  async createUser(input: any) {
+  async createUser(input: CreateUserInput) {
     return UserModel.create(input);
+  }
+
+  async login(input: LoginInput, context: Context) {
+    const e = "Invalid email or password";
+    // get user by email
+    const user = await UserModel.find().findByEmail(input.email).lean();
+
+    if (!user) {
+      throw new ApolloError(e);
+    }
+    // validate the password
+    const passwordIsValid = await bcrypt.compare(input.password, user.password);
+
+    if (!passwordIsValid) {
+      throw new ApolloError(e);
+    }
+    // sign jwt
+    const token = signJwt();
+
+    // set a cookie for the jwt
+
+    // return the jwt
   }
 }
 
